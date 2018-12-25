d675ff00d1d819822a0b9cf703ace52afc5be3fa
root <root@localhost.localdomain>
2018-12-25 15:29:55 +0300

    removing unecessary comment

--- api/Controllers/user_controller.py
+++ api/Controllers/user_controller.py
@@ -109,32 +109,6 @@ def login():
         }), 400
 
 
-# def promote_user_as_admin(user_id):
-#     try:
-#         validator = Validations.empty_user(User.accounts)
-#         if not validator:
-#             for user in User.accounts:
-#                 if user["user_id"] == user_id:
-#                     user["isAdmin"] = True
-#                     return jsonify({
-#                         "status": 200,
-#                         "data": user,
-#                         "message": f"User with user_id {user_id} has been succesfuly promoted as an admin"
-#                     }), 200
-#                 else:
-#                     return jsonify({
-#                         "status": 404,
-#                         "error": "User does not exist"
-#                     }), 404
-#         else:
-#             return jsonify(validator), 404
-
-#     except IndexError:
-#         return jsonify({
-#             'message': 'incident does not exit or check your id',
-#             'status': 404
-#         }), 404
-
 def get_all_users():
     try: 
         if not db.query_all("users"):
